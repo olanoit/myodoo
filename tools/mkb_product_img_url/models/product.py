0021a9aa574baf4587c18ff6b7f1c2ce8ea74559
olanoit <olanoit@gmail.com>
2026-03-17 17:04:37 +0000

    [FIX] mejoras

--- tools/mkb_product_img_url/models/product.py
+++ tools/mkb_product_img_url/models/product.py
@@ -11,34 +11,28 @@ class ProductTemplateInherit(models.Model):
 
     image_url = fields.Char(string='URL de la imagen')
 
-    @api.onchange('image_url')
-    def get_image_from_url(self):
-        """This method mainly use to get image from the url"""
+    def get_image_from_url(self, url):
         image = False
-        if self.image_url:
-            if "http://" in self.image_url or "https://" in self.image_url:
-                image = base64.b64encode(requests.get(self.image_url).content)
+        if url:
+            if url.startswith(('http://', 'https://')):
+                image = base64.b64encode(requests.get(url).content)
             else:
-                with open(self.image_url, 'rb') as file:
+                with open(url, 'rb') as file:
                     image = base64.b64encode(file.read())
-        self.image_1920 = image
-        self.image_url = False
+        return image
 
     @api.model
-    def create(self, values):
-        res = super(ProductTemplateInherit, self).create(values)
-        for img in res:
-            if 'image_url' in values:
-                img.get_image_from_url()
-        return res
+    def create(self, vals):
+        if vals.get('image_url'):
+            vals['image_1920'] = self.get_image_from_url(vals['image_url'])
+            vals['image_url'] = False
+        return super().create(vals)
 
-    def write(self, value):
-
-        rec = super(ProductTemplateInherit, self).write(value)
-        for img in self:  #
-            if 'image_url' in value:
-                img.get_image_from_url()
-        return rec
+    def write(self, vals):
+        if vals.get('image_url'):
+            vals['image_1920'] = self.get_image_from_url(vals['image_url'])
+            vals['image_url'] = False
+        return super().write(vals)
 
 
 class ProductProductInherit(models.Model):
